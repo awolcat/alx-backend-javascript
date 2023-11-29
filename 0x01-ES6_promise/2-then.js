f3743f66e4dcb3900f47db62cc0abf6e533e4e9c
Albert <albertirura11@gmail.com>
2023-11-29 15:19:27 +0300

    fixed promise return by returning promise chain

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
+  return promise
+    .then((response) => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
-    }, () => {
-      console.log('Got a response from the API');
-      return new Error();
     })
     .catch((error = new Error()) => {
-      console.log('Got a response from the API');
       return error;
     });
 }
