07c344cb971a0fd7ff3dc3cee5fcb846b66c6dd8
Albert <albertirura11@gmail.com>
2023-11-29 14:40:07 +0300

    second commit

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,6 +3,9 @@ export default function handleResponseFromAPI(promise) {
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
+    }, () => {
+      console.log('Got a response from the API');
+      return new Error();
     })
     .catch((error = new Error()) => {
       console.log('Got a response from the API');
