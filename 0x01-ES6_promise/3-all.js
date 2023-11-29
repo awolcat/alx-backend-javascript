33b503f2cb437815bdde5718c2fc9582c5ac0660
Albert <albertirura11@gmail.com>
2023-11-29 17:32:51 +0300

    concurrent promises resolve

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,9 +4,10 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   const userData = createUser();
   const photoData = uploadPhoto();
-  Promise.all([photoData, userData]).then(([photoRes, userRes]) => {
-    console.log(photoRes.body, userRes.firstName, userRes.lastName);
-  })
+  Promise.all([photoData, userData])
+    .then(([photoRes, userRes]) => {
+      console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
+    })
     .catch(() => {
       console.log('Signup system offline');
     });
