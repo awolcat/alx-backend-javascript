9538263c12832690e9739405969f7b8768bc6402
Albert <albertirura11@gmail.com>
2023-12-07 19:18:48 +0300

    fix lint errors

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
-      return [];
+    return [];
   }
   const mappedArray = array.map((obj) => obj.id);
   return mappedArray;
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,8 @@ export default function updateStuentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      studentGrade = gradeObj === undefined ? 'N/A' : gradeObj.grade;
-      return {...student, grade};
-    })
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const studentGrade = gradeObj === undefined ? 'N/A' : gradeObj.grade;
+      return { ...student, studentGrade };
+    });
 }
